550f4d6ee2beabb72d828daec96c1f14bb979103
mikesuchor <mikesuchor@gmail.com>
2019-04-25 01:10:54 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        variableWidth: true,
+        arrows: false
+    });
+});
+
+// When the user scrolls the page, execute checkSticky function
+window.onscroll = function() {checkSticky()};
+
+// Get the navbar
+var navbar = document.getElementById("site-navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function checkSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
